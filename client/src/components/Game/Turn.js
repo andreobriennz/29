f1e8fb742265350b6658990535416c7b57db2265
andreobriennz <andre@communica.co.nz>
2018-03-27 18:18:18 +1300

    Logic for handling each new turn extracted into its own class

---
+++ client/src/components/Game/Turn.js
@@ -0,0 +1,22 @@
+class Turn {
+    constructor (state) {
+        this.state = state
+    }
+
+    nextTurn  () {
+        let round = this.state.round
+        let currentPlayer = ++this.state.currentPlayer
+
+        if (currentPlayer > this.state.players.length) {
+            currentPlayer = 0
+            ++round
+        }
+
+        return {
+            round: round,
+            currentPlayer: currentPlayer
+        }
+    }
+}
+
+export {Turn}
