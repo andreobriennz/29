56dcc222ad9e401a0fed842794a990a0ffaa7549
andreobriennz <andre@communica.co.nz>
2018-03-28 20:04:14 +1300

    Use props so component updates, use es6 fat arrow syntax to fix scoping issue

--- client/src/components/Game/Cards/Cards.js
+++ client/src/components/Game/Cards/Cards.js
@@ -1,30 +1,23 @@
 import React from 'react'
 import { Card } from './Card'
+import {info} from './../../Info'
 
 class Cards extends React.Component {
-    constructor (props) {
-        super (props)
-
-        this.state = {
-            cards: this.props.cards,
-        }
-    }
 
     render() {
-        let _this = this
         return (
             <section className="cards">
                 <strong className="large">Your Cards</strong>
-
+                
                 <div className="cards">
-                    {this.state.cards.map(function(card, index) {
+                    {this.props.cards.map ( (card, index) => {
                         let description = `${description}          `
                         description = card.description.match(/\b[\w']+(?:[^\w\n]+[\w']+){0,1}\b/g)
 
                         return (
                             <div
                             key={index} 
-                            onClick={_this.props.handlePlayCard.bind(this, card)}
+                            onClick={this.props.handlePlayCard.bind(this, card)}
                             className="card">
                                 <Card title={card.name} description={description} footer={card.effects} />
                             </div>
@@ -33,7 +26,7 @@ class Cards extends React.Component {
                 </div>
             </section>
         );
-      }
+    }
 };
 
 export { Cards }
