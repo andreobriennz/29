3e56f4418d1b8d7cb5055e787306a845cddf817c
andreobriennz <andre@communica.co.nz>
2018-03-28 20:03:07 +1300

    Let Action class accept new way of building deck, fix wrong card being removed and change = to ===

--- client/src/components/Game/Action.js
+++ client/src/components/Game/Action.js
@@ -1,4 +1,5 @@
 import {Deck} from './Cards/Deck'
+import {info} from './../Info'
 
 class Action {
     constructor (state) {
@@ -8,29 +9,26 @@ class Action {
     pickUp (currentPlayer = this.state.currentPlayer, howMany = 1) {
         let players = this.state.players
         let deck = this.state.deck
-        console.log(deck)
 
         if (typeof this.state.deck != 'undefined') {
             deck = this.state.deck
         }
         else {
-            // deck = new Deck ().shuffleDeck ()
-            console.error ('Outta cards. Shuffle')
-            return
+            deck = Deck.buildDeck ()
         }
         
         for (let i = 0; i < howMany; i++) {
             let index = (
                 Math.round(
-                    Math.random() * (deck.cards.length - 1)
+                    Math.random() * (deck.length - 1)
                 )
             );
         
-            let card = deck.cards[index]
+            let card = deck[index]
 
             // add to player index, default to current player
             players[currentPlayer].cards.push (card)
-            deck.cards.splice (index, 1)
+            deck.splice (index, 1)
         }
 
         return {
@@ -42,7 +40,7 @@ class Action {
     playCard (card) {
         let players = this.state.players
         let currentPlayer = this.state.currentPlayer
-        let opponent = (this.state.currentPlayer = 0 ? 1 : 0)
+        let opponent = (this.state.currentPlayer === 0 ? 1 : 0)
 
         // get cards effects, and update players with effects
         const effect = card.effect (this.state.players[currentPlayer], this.state.players[opponent])
@@ -51,7 +49,7 @@ class Action {
 
         // remove card
         const index = this.state.players[currentPlayer].cards.indexOf (card)
-        players[currentPlayer].cards = this.state.players[currentPlayer].cards.splice (index, 1)
+        players[currentPlayer].cards.splice (index, 1)
 
         return {
             players: players
