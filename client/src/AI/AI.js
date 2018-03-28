890af6260fc924e73ee2ea5bb503701ccbd2b4a3
andreobriennz <andre@communica.co.nz>
2018-03-28 20:00:04 +1300

    Fix issues in AI class and improve weights

--- client/src/AI/AI.js
+++ client/src/AI/AI.js
@@ -37,12 +37,12 @@ class AI {
 
         let want = []
         let kindaWant = []
-        for (let i = 0; i < this.cards.length; i++) {
-            if (this.cards[i].id === best) {
-                want.push(this.cards[i]) // choose card to play
+        for (let i = 0; i < this.cards.cards.length; i++) {
+            if (this.cards.cards[i].id === best) {
+                want.push(this.cards.cards[i]) // choose card to play
             }
-            else if (this.need[this.cards[i].id] > max - 10) {
-                kindaWant.push(this.cards[i])
+            else if (this.need[this.cards.cards[i].id] > max - 10) {
+                kindaWant.push(this.cards.cards[i])
             }
         }
 
@@ -50,10 +50,10 @@ class AI {
 
         info.event ('AI Decision', [this.need, best, want])
 
-        if (want !== []) return want
-
-        // prob better to return want = [] 
-        return false // choose to pick up card
+        info.log ('needs:')
+        console.log (this.need)
+        
+        return want
     }
 
     determineNeeds (players) {
@@ -64,8 +64,8 @@ class AI {
         let opponentWithShelter = 100 - opponentWithoutShelter
 
         this.need.shelter = percentWithoutShelter (
-            this.state.players[this.user].population,
-            this.state.players[this.user].shelters
+            this.state.players[this.state.currentPlayer].stats.population,
+            this.state.players[this.state.currentPlayer].stats.shelters
         )
 
         this.need.bomber = opponentWithShelter
@@ -74,151 +74,25 @@ class AI {
     }
 
     weights () {
-        this.need.swarm
+        // this.need.swarm
 
-        this.need.bomber
-        if (this.state.players[this.opponent].stats.shelter < 1) {
+        // this.need.bomber
+        this.need.bomber += 10
+        if (this.state.players[this.opponent].stats.shelter < (this.state.players[this.opponent].stats.population * 10) 
+            || this.state.players[this.opponent].stats.shelter == undefined) {
             this.need.bomber = 0
+            info.error ('0 bomber need')
         }
 
         // this.need.shelter
-        // if ((this.need.shelter * 10) > (this.state.players[this.opponent].stats.population * 1.2)) {
-        //     this.need.shelter = this.need.shelter / 2
-        // }
+        this.need.shelter += 30
+        if (this.state.players[this.user].stats.population <= 60) {
+            this.need.shelter = this.need.shelter * 1.6
+        }
+        if ((this.state.players[this.user].stats.shelter * 10) > (this.state.players[this.user].stats.population * 1.2)) {
+            this.need.shelter = this.need.shelter * 0.8
+        }
     }
 }
 
 export {AI}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class AI {
-//     // log all lessons, go over them to 'calibrate', 
-//     // then each time returns weights AND alter weights for every relevant situation 
-//     // (eg same user, same playing style, etc)
-//     constructor (currentPlayer, players) {
-//         this.learningMode = true
-
-//         this.currentPlayer
-//         this.players = players
-
-//         this.user = players[currentPlayer]
-//         // this.opponents = players.splice(currentPlayer, 1)
-
-//         this.goal = this.user
-
-//         console.log ('goal',this.goal)
-//     }
-//     ////////////////
-
-//     decide () {        
-//         // if (this.learningMode = true) this.tryNewMove ()
-//         console.log(this.players[this.currentPlayer])
-//     }
-
-//     /////////////////
-
-//     evaluateResult() {
-        
-//     }
-
-//     tryNewMove () {
-//         const index = Math.floor(Math.random() * Math.floor(deck.cards.length))
-//         const pickUp = deck.cards[index]
-//     }
-
-//     // newLesson (lesson = {
-
-//     // }) {
-//     //     this.lessons.push (lesson)
-//     // }
-
-
-// };
-
-// // new instance for each card
-
-// export { AI }
-
-
-
-
-
-
-
-// // say () {
-// //     // eg 'Wow, you play quite aggressively!'
-// //     // eg1 'You dont play as aggressively as some people. I hope that doesnt mean youre stockpiling cards'
-// //     // eg3 'Come
-// // }
-
-
-
-// // this.lessons = [
-// //     // save what was learnt each time (add to bias next time, and add more stringly if applicable to this player)
-// //     // eg:
-// //     {
-// //         who: '',
-// //         what: 'lost', // won, lost, tried something new, realised
-// //         where: '', // ?
-// //         why: '',
-// //         how: '', // ?
-// //         realisedAtRound: 10,
-// //         weight: 10 // how strongly to remember this
-        
-// //     }
-// // ]
-
-
-
-//         // this.opponents.playingStyles = {
-//         //     // aggressiveness: 0
-//         // }
