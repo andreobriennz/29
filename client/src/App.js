3979f18cbdbe07dd6fc60ff231742bcf84d7820d
andreobriennz <andre@communica.co.nz>
2018-04-05 17:57:27 +1200

    Test change path, and add react-script

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 // import logo from './logo.svg'
 import './App.css'
 
-import {Header} from './components/Layout/Header'
+import {Header} from 'components/Layout/Header'
 import {Game} from './components/Game/Game'
 
 class App extends Component {
